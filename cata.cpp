d482430455fe4b6de9a6998032e428858e180a16
devmam999 <107886293+devmam999@users.noreply.github.com>
2023-03-25 19:08:17 -0700

    Update cata.cpp

--- cata.cpp
+++ cata.cpp
@@ -8,7 +8,7 @@ void setCata(int power) {
 //set Catapult Motor
 int cataPower = 0;
 void setCataMotor() {
-    if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_R1)) {
+    if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_R1)) {
         if (limitSwitch.get_value()) {
             cataPower = 127;
         }
