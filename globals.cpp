14be4a24315b63e0fa49efdc54d4774e178612e8
devmam999 <107886293+devmam999@users.noreply.github.com>
2023-03-26 10:54:07 -0700

    Update globals.cpp

--- globals.cpp
+++ globals.cpp
@@ -3,8 +3,11 @@
 //intake and cata
 pros::Motor intake(1, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_COUNTS);
 pros::Motor cata(2, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor_Group intakeCata({intake, cata});
 //limitSwitch
 pros::ADIDigitalIn limitSwitch('2');
 //controller
 pros::Controller controller(pros::E_CONTROLLER_MASTER);
+
+
 
